055e136194ad90356fc4b1fa7c04daef248a2caf
Chong Lian <alexchonglian@gmail.com>
2014-03-24 20:22:15 -0400

    add loggedin.html page

--- app/js/app.js
+++ app/js/app.js
@@ -13,5 +13,7 @@ angular.module('dormCatApp', [
 config(['$routeProvider', function($routeProvider) {
   $routeProvider.when('/login', {templateUrl: 'partials/login.html', controller: 'LoginCtrl'});
   $routeProvider.when('/signup', {templateUrl: 'partials/signup.html', controller: 'SignupCtrl'});
+  $routeProvider.when('/dash', {templateUrl: 'partials/dashboard.html', controller: 'DashCtrl'});
+  $routeProvider.when('/bug', {templateUrl: 'partials/bugreport.html', controller: 'BugCtrl'});
   $routeProvider.otherwise({redirectTo: '/'});
 }]);
